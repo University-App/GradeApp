eeded74ac4e4924873faab1bcbcf6d40846d71e2
Paul-Marie DJEKINNOU <43011957+PaulmarieDjekinnou@users.noreply.github.com>
2022-10-09 01:08:40 +0200

    #13 #12 Taskfile, migraiton file and sqlc file creation (#17)

---
+++ docker/database/migration/000001_init_schema.up.sql
@@ -0,0 +1,37 @@
+CREATE TABLE "Grade" (
+                         "id" SERIAL PRIMARY KEY,
+                         "number" int NOT NULL,
+                         "course_name" varchar NOT NULL,
+                         "student_id" int
+);
+
+CREATE TABLE "Unite" (
+                         "id" SERIAL PRIMARY KEY,
+                         "unite_name" varchar NOT NULL
+);
+
+CREATE TABLE "Course" (
+                          "id" SERIAL PRIMARY KEY,
+                          "course_name" varchar NOT NULL,
+                          "unite_id" int
+);
+
+CREATE TABLE "Student" (
+                           "id" SERIAL PRIMARY KEY,
+                           "last_name" varchar NOT NULL,
+                           "first_name" varchar NOT NULL
+);
+
+ALTER TABLE "Grade" ADD FOREIGN KEY ("student_id") REFERENCES "Student" ("id");
+
+ALTER TABLE "Course" ADD FOREIGN KEY ("unite_id") REFERENCES "Unite" ("id");
+
+CREATE TABLE "Student_Course" (
+                                  "Student_id" int NOT NULL,
+                                  "Course_id" int NOT NULL,
+                                  PRIMARY KEY ("Student_id", "Course_id")
+);
+
+ALTER TABLE "Student_Course" ADD FOREIGN KEY ("Student_id") REFERENCES "Student" ("id");
+
+ALTER TABLE "Student_Course" ADD FOREIGN KEY ("Course_id") REFERENCES "Course" ("id");
