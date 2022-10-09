9f64f7236c59669b7ae1aa330a552546613fb0cb
PaulmarieDjekinnou <paulmarie.djekinnou@outlook.fr>
2022-10-09 13:28:31 +0200

    #3 [TECH] Initialisation des données

--- docker/database/migration/dev/000001_init_schema.up.sql
+++ docker/database/migration/dev/000001_init_schema.up.sql
@@ -35,3 +35,40 @@ CREATE TABLE "Student_Course" (
 ALTER TABLE "Student_Course" ADD FOREIGN KEY ("Student_id") REFERENCES "Student" ("id");
 
 ALTER TABLE "Student_Course" ADD FOREIGN KEY ("Course_id") REFERENCES "Course" ("id");
+
+----------------------------------------------------------------------------------------
+/*
+    SEEDING DEV DATABASE
+ */
+
+INSERT INTO "Student" (id, last_name, first_name) VALUES (1, 'Djek', 'Pm');
+INSERT INTO "Student" (id, last_name, first_name) VALUES (2, 'Djek', 'test');
+INSERT INTO "Student" (id, last_name, first_name) VALUES (3, 'Djek', 'test1');
+
+INSERT INTO "Unite" (id, unite_name) VALUES (1, 'UE1');
+INSERT INTO "Unite" (id, unite_name) VALUES (2, 'UE2');
+
+INSERT INTO "Course" (id, course_name, unite_id) VALUES (1, 'Course1', 1);
+INSERT INTO "Course" (id, course_name, unite_id) VALUES (2, 'Course3', 1);
+INSERT INTO "Course" (id, course_name, unite_id) VALUES (3, 'Course2', 2);
+INSERT INTO "Course" (id, course_name, unite_id) VALUES (4, 'Course4', 2);
+
+INSERT INTO "Grade" (id, grade_number, course_name, student_id) VALUES (1, 10, 'Course1', 1);
+INSERT INTO "Grade" (id, grade_number, course_name, student_id) VALUES (2, 15, 'Course1', 2);
+INSERT INTO "Grade" (id, grade_number, course_name, student_id) VALUES (3, 20, 'Course1', 1);
+INSERT INTO "Grade" (id, grade_number, course_name, student_id) VALUES (4, 13, 'Course1', 2);
+
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (1, 1);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (1, 2);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (1, 3);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (1, 4);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (2, 1);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (2, 2);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (2, 3);
+INSERT INTO "Student_Course" ("Student_id", "Course_id") VALUES (2, 4);
+
+
+ALTER SEQUENCE "Student_id_seq" RESTART WITH 4;
+ALTER SEQUENCE "Unite_id_seq" RESTART WITH 3;
+ALTER SEQUENCE "Course_id_seq" RESTART WITH 5;
+ALTER SEQUENCE "Grade_id_seq" RESTART WITH 5;
